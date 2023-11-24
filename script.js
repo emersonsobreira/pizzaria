5bd33e6dbd7afefa051044e9c88c22515c526034
emersonsobreira <124679545+emersonsobreira@users.noreply.github.com>
2023-11-23 22:17:16 -0300

    ultima versão

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let interacao = true;
+const menuContant = document.querySelector(' .content');
+const menuToggle = menuContant.querySelector('.menu-toggle');
 
+menuToggle.addEventListener('click', () =>{
+    menuContant.classList.toggle('on', interacao);
+    interacao =!interacao;
+
+    
+});
